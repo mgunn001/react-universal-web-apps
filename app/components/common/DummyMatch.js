3ac02d3904a5b9df2d7f63f42bb2822bdbf663c5
Maheedhar Gunnam <maheedhargunnam@gmail.com>
2017-10-14 20:26:46 -0400

    Some more experimentation

---
+++ app/components/common/DummyMatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class DummyMatch extends React.Component {
+    render() {
+        return (
+            <section className="app-content">
+                <header className="section-header">
+                    <h3 className="title">Dummy Match Page_Added For Testing</h3>
+                </header>
+            </section>
+        );
+    }
+}
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ import {IndexRoute, Route} from 'react-router';
 
 import App from './components/app';
 import NoMatch from './components/common/NoMatch';
+import DummyMatch from './components/common/DummyMatch';
 
 import Dashboard from './components/dashboard/Dashboard';
 import LatestBills from './components/bill/LatestBills';
@@ -12,6 +13,7 @@ export default (
         <Route component={Dashboard}>
             <IndexRoute component={LatestBills}/>
         </Route>
-        <Route path="*" component={NoMatch}/>
+        <Route path="nomatch" component={NoMatch}/>
+        <Route path="dummymatch" component={DummyMatch}/>
     </Route>
 );
